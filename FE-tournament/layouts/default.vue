bc1be1ef7b4425bfb03cd220e824de0be998b005
tbnkhanh <tranbuinhukhanh@gmail.com>
2024-03-15 17:28:37 +0700

    m

--- FE-tournament/layouts/default.vue
+++ FE-tournament/layouts/default.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import {useAuthStore} from "~/stores/useAuthStore";
+import { useAuthStore } from "~/stores/useAuthStore";
 
 const auth = useAuthStore();
 
@@ -16,27 +16,40 @@ async function handleLogout() {
     <pre>{{ auth.user }}</pre> -->
 
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
-      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4" style="max-width: 600px">
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4" style="max-width: 488px">
 
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul
+            class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <NuxtLink class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="/">Home</NuxtLink>
+              <NuxtLink
+                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                to="/">Home</NuxtLink>
             </li>
             <li>
-              <NuxtLink class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="/register">Register</NuxtLink>
+              <NuxtLink
+                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                to="/register">Register</NuxtLink>
             </li>
             <li>
-              <NuxtLink class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="/login">Login</NuxtLink>
+              <NuxtLink
+                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                to="/auth-only">Auth only</NuxtLink>
             </li>
             <li>
-              <NuxtLink class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="/auth-only">Auth only</NuxtLink>
+              <NuxtLink
+                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                to="/guest-only">Guest only</NuxtLink>
             </li>
-            <li>
-              <NuxtLink class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="/guest-only">Guest only</NuxtLink>
+            <li v-if="!auth.isLoggedIn">
+              <NuxtLink
+                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                to="/login">Login</NuxtLink>
             </li>
             <li v-if="auth.isLoggedIn" @click="handleLogout">
-              <NuxtLink class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="/guest-only">Log out</NuxtLink>
+              <NuxtLink
+                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                to="/guest-only">Log out</NuxtLink>
             </li>
           </ul>
         </div>
--- FE-tournament/pages/login.vue
+++ FE-tournament/pages/login.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import {useAuthStore} from "~/stores/useAuthStore";
+import { useAuthStore } from "~/stores/useAuthStore";
 
 const form = ref({
   email: "admin@gmail.com",
@@ -9,14 +9,14 @@ const form = ref({
 const auth = useAuthStore();
 
 async function handleLogin() {
-  const {error} = await auth.login(form.value);
+  const { error } = await auth.login(form.value);
 
   console.log(error);
 }
 </script>
 
 <template>
-  <form @submit.prevent="handleLogin">
+  <!-- <form @submit.prevent="handleLogin">
     <label>
       Email
       <input v-model="form.email" type="email">
@@ -27,7 +27,44 @@ async function handleLogin() {
     </label>
 
     <button>Login</button>
+  </form> -->
+  <form @submit.prevent="handleLogin">
+    <section class="bg-gray-50 dark:bg-gray-900" style="margin-top: -56px">
+      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div
+          class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Sign in to your account
+            </h1>
+            <form class="space-y-4 md:space-y-6" action="#">
+              <div>
+                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                  email</label>
+                <input type="email" name="email" id="email"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com">
+              </div>
+              <div>
+                <label for="password"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                <input type="password" name="password" id="password" placeholder="••••••••"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              </div>
+              <button type="submit"
+                class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800;"
+                style=" background-color: steelblue">Sign in</button>
+              <div class="text-sm font-light text-gray-500 dark:text-gray-400">
+                Don’t have an account yet? 
+                <NuxtLink to="/register">Register</NuxtLink>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   </form>
+
 </template>
 
 <style scoped></style>
